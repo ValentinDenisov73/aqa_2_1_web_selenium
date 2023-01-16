9cf8ed4039f0a662c1e9a2396e8b31bcdab700b4
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-16 16:34:35 +0400

    Initial commit

---
+++ src/test/java/CallbackTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void  tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTest(){
+        throw new UnsupportedOperationException();
+    }
+}
+
+//    ChromeOptions options = new ChromeOptions();
+//options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
