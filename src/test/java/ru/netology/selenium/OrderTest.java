dc959f91bbe9c31e1c1cb3c08e65b895d5df064e
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-16 23:32:48 +0400

    add test

--- src/test/java/ru/netology/selenium/OrderTest.java
+++ src/test/java/ru/netology/selenium/OrderTest.java
@@ -1,22 +1,34 @@
 package ru.netology.selenium;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
     private WebDriver driver;
 
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -27,7 +39,16 @@ public class OrderTest {
 
     @Test
     void shouldTest() {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+       driver.get("http://localhost:9999/");
+       driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+       driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+       driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+       driver.findElement(By.className("button__text")).click();
+       String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+       String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+       assertEquals(expected,actual);
+
     }
 }
 
