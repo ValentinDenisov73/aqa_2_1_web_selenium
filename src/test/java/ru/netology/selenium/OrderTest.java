1c24b854c5d6f08b18914e02fb98924a81306f21
Valentin Denisov <valentindenisov1989@gmail.com>
2023-01-16 20:24:13 +0400

    add driver

---
+++ src/test/java/ru/netology/selenium/OrderTest.java
@@ -0,0 +1,33 @@
+package ru.netology.selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class OrderTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTest() {
+        throw new UnsupportedOperationException();
+    }
+}
+
